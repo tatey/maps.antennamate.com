bbd13352a4e2e350da6a285470b1d11f87416414
Tate Johnson <tate@tatey.com>
2014-01-11 21:43:34 +1100

    Share center lat/lng with controllers.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,47 @@
 //= require angular
 
-var app = angular.module('app', []);
+var app = angular.module('am', []);
 
-app.directive('amGoogleMap', [function() {
+app.factory('Location', [function() {
   return {
+    center: {
+      lat: -34.397,
+      lng: 150.644
+    }
+  };
+}]);
+
+app.controller('MapController', ['Location', function(Location) {
+  this.location = Location;
+}]);
+
+app.controller('TransmittersController', ['Location', function(Location) {
+  this.location = Location;
+}]);
+
+app.directive('googleMap', ['$timeout', function($timeout) {
+  return {
+    scope: {
+      'center': '='
+    },
+
     link: function(scope, el, attrs) {
       var options = {
         zoom: 8,
-        center: new google.maps.LatLng(-34.397, 150.644)
+        center: new google.maps.LatLng(scope.center.lat, scope.center.lng)
       };
 
       var map = new google.maps.Map(el[0], options);
+
+      google.maps.event.addListener(map, 'center_changed', function() {
+        $timeout(function() {
+          var center = map.getCenter();
+          scope.center = {
+            lat: center.lat(),
+            lng: center.lng()
+          };
+        });
+      });
     }
   };
 }]);

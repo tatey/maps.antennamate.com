2823795d8751622cc5c2691b674095de213d8db8
Tate Johnson <tate@tatey.com>
2014-01-11 20:44:34 +1100

    Vendor bootstrap.

--- plugins/assets.rb
+++ plugins/assets.rb
@@ -1,3 +1,4 @@
 require 'jekyll-assets'
 
 Sprockets.append_path File.expand_path('../../vendor/assets/javascripts', __FILE__)
+Sprockets.append_path File.expand_path('../../vendor/assets/stylesheets', __FILE__)

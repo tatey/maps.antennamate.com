2efb341325e6f1860809d2deff44502f5b7c0fd3
Tate Johnson <tate@tatey.com>
2014-01-13 00:10:26 +1100

    Show sites in left hand pane.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,35 +12,36 @@ app.factory('Location', [function() {
   };
 }]);
 
-app.factory('Transmitter', ['$http', function($http) {
+app.factory('SiteResource', ['$http', function($http) {
   var _radians;
 
-  var Transmitter = function(props) {
+  var SiteResource = function(props) {
     angular.extend(this, props);
     return this;
   };
 
-  Transmitter.nearby = function(position) {
-    return this.query().then(function(transmitters) {
-      return _.filter(transmitters, function(transmitter) {
-        return transmitter.isNear(position);
+  SiteResource.nearby = function(position) {
+    return this.query().then(function(sites) {
+      return _.filter(sites, function(site) {
+        return site.isNear(position);
       });
     });
   };
 
-  Transmitter.query = function() {
+  SiteResource.query = function() {
     return $http({
       url: '/data.json',
-      method: 'GET'
+      method: 'GET',
+      cache: true
     })
     .then(function(response) {
       return _.map(response.data, function(props) {
-        return new Transmitter(props);
+        return new SiteResource(props);
       });
     });
   };
 
-  Transmitter.prototype.isNear = function(position) {
+  SiteResource.prototype.isNear = function(position) {
     return (Math.acos(Math.sin(_radians(this.latitude)) * Math.sin(_radians(position.lat)) + Math.cos(_radians(this.latitude)) * Math.cos(_radians(position.lat)) * Math.cos(_radians(position.lng) - _radians(this.longitude))) * 6378100) <= 150000;
   };
 
@@ -48,7 +49,7 @@ app.factory('Transmitter', ['$http', function($http) {
     return degrees * Math.PI / 180.0;
   };
 
-  return Transmitter;
+  return SiteResource;
 }]);
 
 app.controller('MapController', ['Location', function(Location) {
@@ -56,12 +57,18 @@ app.controller('MapController', ['Location', function(Location) {
   this.markers = [{position: {lat: -34.397, lng: 150.644}}];
 }]);
 
-app.controller('TransmittersController', ['Location', 'Transmitter', function(Location, Transmitter) {
-  this.location = Location;
-  this.transmitters = Transmitter.nearby(this.location.center);
+app.controller('SitesController', ['$scope', 'Location', 'SiteResource', function($scope, Location, SiteResource) {
+  $scope.location = Location;
+  $scope.sites = [];
+
+  $scope.$watch('location.center', function(center) {
+    SiteResource.nearby(center).then(function(sites) {
+      $scope.sites = sites;
+    });
+  });
 }]);
 
-app.directive('googleMap', ['$document', '$timeout', function($document, $timeout) {
+app.directive('googleMap', ['$timeout', function($timeout) {
   return {
     scope: {
       'center': '='

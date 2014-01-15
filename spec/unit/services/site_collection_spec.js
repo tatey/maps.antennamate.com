13e913ec09351c6023ad32f0af0b70b7ee981821
Tate Johnson <tate@tatey.com>
2014-01-15 11:52:31 +1100

    More specs for SiteCollection.

--- spec/unit/services/site_collection_spec.js
+++ spec/unit/services/site_collection_spec.js
@@ -9,7 +9,31 @@ describe('SiteCollection', function() {
     });
   });
 
-  describe('#openSite', function() {
+  describe('.findById', function() {
+    it('it returns the site with the id matching the given argument', function() {
+      var site1, site2, site3;
+
+      site1 = {id: 1};
+      site2 = {id: 2};
+      site3 = {id: 3};
+
+      SiteCollection.sites = [site1, site2, site3];
+
+      expect(SiteCollection.findById(3)).toEqual(site3);
+    });
+  });
+
+  describe('.closeSite', function() {
+    it('sets open to false', function() {
+      var oldSite = {open: true};
+
+      SiteCollection.closeSite(oldSite);
+
+      expect(oldSite.open).toBeFalsy();
+    });
+  });
+
+  describe('.openSite', function() {
     var newSite, oldSite;
 
     beforeEach(function() {

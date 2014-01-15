7afd840f824b16dd5c9e4694be81039b1d49628f
Tate Johnson <tate@tatey.com>
2014-01-15 16:37:09 +1100

    Change default location.

--- app/javascripts/services/location.js
+++ app/javascripts/services/location.js
@@ -3,8 +3,8 @@ var app = angular.module('am');
 app.factory('Location', [function() {
   return {
     center: {
-      lat: -34.397,
-      lng: 150.655
+      lat: -33.819,
+      lng: 150.586 // Sydney
     },
 
     setCenter: function(newCenter) {

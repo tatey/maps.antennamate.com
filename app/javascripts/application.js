c823298ef19c1e67b023bf49071c52203638d700
Ash Kyd <ash@kyd.com.au>
2014-01-18 14:33:28 +1000

    Added some really basic feature detection and a fallback message for old, awful browsers.

--- app/javascripts/application.js
+++ app/javascripts/application.js
@@ -8,6 +8,19 @@
 
 
 (function(){
+  // Before we start, let's make sure we're using a modern browser.
+  var FALLBACK_MESSAGE = '<p>Your browser is too old to support this website. Please '+
+    '<a href="http://browsehappy.com/">upgrade your browser</a> or install the app.</p>';
+
+  // Angular requires JSON, this seems to catch most browsers. We could add more
+  // tests here as required.
+  if(!window.JSON){
+  	window.onload = function(){
+  		document.getElementById('messages').innerHTML = FALLBACK_MESSAGE;
+  	}
+    return;
+  }
+
   angular.module('am', []);
 
   // Since we have Google Loader, load Google Maps with it instead of hard-coding

039971186b1f6a8861a271858da085ea773f8395
Ash Kyd <ash@kyd.com.au>
2014-01-18 14:32:53 +1000

    Added the Google loader so we can load Maps and use Google's server-side location detection.

--- app/javascripts/application.js
+++ app/javascripts/application.js
@@ -6,4 +6,17 @@
 //= require_tree ./filters
 //= require_tree ./services
 
-angular.module('am', []);
+
+(function(){
+  angular.module('am', []);
+
+  // Since we have Google Loader, load Google Maps with it instead of hard-coding
+  // in the document.
+  google.load("maps", "3", {
+    other_params:'sensor=false',
+    callback: function(){
+      // Initialize our app once it's done.
+      angular.bootstrap(document.querySelector('body'), ['am']);
+    }
+  });
+})()
--- app/javascripts/services/location.js
+++ app/javascripts/services/location.js
@@ -1,11 +1,23 @@
 var app = angular.module('am');
 
 app.factory('Location', [function() {
-  return {
-    center: {
+  var center;
+
+  // Where possible, use Google's IP-based geolocation
+  if(window.google && google.loader && google.loader.ClientLocation && google.loader.ClientLocation.latitude){
+    center = {
+      lat: google.loader.ClientLocation.latitude,
+      lng: google.loader.ClientLocation.longitude
+    };
+  } else {
+    center = {
       lat: -33.819,
       lng: 150.586 // Sydney
-    },
+    };
+  }
+
+  return {
+    center: center,
 
     setCenter: function(newCenter) {
       this.center = newCenter;
